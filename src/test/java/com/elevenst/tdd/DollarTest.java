498a00302875b8d88cd988803cde798b3ebb806c
gyugyu90 <kh2000park@gmail.com>
2020-08-05 18:07:20 +0900

    5장 예제

--- src/test/java/com/elevenst/tdd/DollarTest.java
+++ src/test/java/com/elevenst/tdd/DollarTest.java
@@ -10,10 +10,8 @@ public class DollarTest {
     @Test
     public void testMultiplication() {
         Dollar five = new Dollar(5);
-
         assertEquals(new Dollar(10), five.times(2));
         assertEquals(new Dollar(15), five.times(3));
-
     }
 
     @Test
@@ -22,5 +20,11 @@ public class DollarTest {
         assertFalse(new Dollar(5).equals(new Dollar(6)));
     }
 
+    @Test
+    public void testFrancMultiplication() {
+        Franc five = new Franc(5);
+        assertEquals(new Franc(10), five.times(2));
+        assertEquals(new Franc(15), five.times(3));
+    }
 
 }
