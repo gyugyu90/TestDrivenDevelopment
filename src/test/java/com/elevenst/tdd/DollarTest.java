c3c0f0d2e95bf4b277252928cc11301992e2ec72
gyugyu90 <kh2000park@gmail.com>
2020-08-05 17:40:04 +0900

    1장 예제

---
+++ src/test/java/com/elevenst/tdd/DollarTest.java
@@ -0,0 +1,17 @@
+package com.elevenst.tdd;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.*;
+
+
+public class DollarTest {
+
+    @Test
+    public void testMultiplication() {
+        Dollar five = new Dollar(5);
+        five.times(2);
+        assertEquals(10, five.amount);
+    }
+
+}
