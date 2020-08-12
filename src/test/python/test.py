8284b59de691aff700e2bf94b6eac838ac819c75
gyugyu90 <kh2000park@gmail.com>
2020-08-12 21:26:41 +0900

    22장 예제

--- src/test/python/test.py
+++ src/test/python/test.py
@@ -7,10 +7,13 @@ class TestCase:
         pass
 
     def run(self):
+        result = TestResult()
+        result.testStarted()
         self.setUp()
         method = getattr(self, self.name)
         method()
         self.tearDown()
+        return result
 
     def tearDown(self):
         pass
@@ -33,6 +36,20 @@ class WasRun(TestCase):
     def tearDown(self):
         self.log = self.log + "tearDown "
 
+    def testBrokenMethod(self):
+        raise
+
+
+class TestResult:
+    def __init__(self):
+        self.runCount = 0
+
+    def testStarted(self):
+        self.runCount = self.runCount + 1
+
+    def summary(self):
+        return "%d run, 0 failed" % self.runCount
+
 
 class TestCaseTest(TestCase):
     def setUp(self):
@@ -43,6 +60,16 @@ class TestCaseTest(TestCase):
         test.run()
         assert("setUp testMethod tearDown " == test.log)
 
+    def testResult(self):
+        test = WasRun("testMethod")
+        result = test.run()
+        assert("1 run, 0 failed" == result.summary())
+
+    def testFailedResult(self):
+        test = WasRun("testBrokenMethod")
+        result = test.run()
+        assert("1 run, 1 failed" == result.summary())
 
 # TestCaseTest("testRunning").run()
-TestCaseTest("testTemplateMethod").run()
+TestCaseTest("testResult").run()
+print("hello world")
